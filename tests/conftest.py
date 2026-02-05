97b2e96479f6e1a3f41cda1cfd903a285bd56dd1
SAR <sar@intetics.com>
2026-02-05 23:38:56 +0100

    ci: New env for BS

--- tests/conftest.py
+++ tests/conftest.py
@@ -100,11 +100,11 @@ def driver(request):
     options = UiAutomator2Options ()
     options.platform_name = "Android"
 
-
-
     if env == "browserstack":
+        options.set_capability ("platformName", "Android")
+        options.set_capability ("appium:automationName", "UiAutomator2")
+        options.set_capability ("appium:app", "bs://7b0adfefe5d9ba755b2a21e3d1da38fa3436e079")
 
-        options.set_capability ("app", "bs://7b0adfefe5d9ba755b2a21e3d1da38fa3436e079")
         options.set_capability ("bstack:options", {
             "userName": os.getenv ("BROWSERSTACK_USERNAME"),
             "accessKey": os.getenv ("BROWSERSTACK_ACCESS_KEY"),
@@ -113,9 +113,9 @@ def driver(request):
             "projectName": "Python Appium Tests",
             "buildName": "GitHub Actions Build",
             "sessionName": "Mobile tests",
-
         })
-        remote_url = "https://hub.browserstack.com/wd/hub"
+
+        remote_url = "https://hub-cloud.browserstack.com/wd/hub"
 
     else:
         options.device_name = "emulator-5554"
