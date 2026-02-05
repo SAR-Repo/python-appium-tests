0592b5dc7b18a203c6672889b5835dcc42a70a2f
SAR <sar@intetics.com>
2026-02-05 13:53:43 +0100

    ci: pytestmark = pytest.mark.manual

--- test_manual.py
+++ test_manual.py
@@ -1,11 +1,14 @@
 import pytest
+pytestmark = pytest.mark.manual
 
-@pytest.mark.manual
+# @pytest.mark.manual
 def test_manual_examlpe_1():
-    assert 1+1==2
 
+    assert 1+1==2
+# @pytest.mark.manual
 def test_manual_examlpe_2():
-    assert 1+1==2
 
+    assert 1+1==2
+# @pytest.mark.manual
 def test_manual_examlpe_3():
     assert 1+1==2
