cd4b5bc0e46faf87fe9bbd09a9afe8e8b556e9c5
SAR <sar@intetics.com>
2026-02-05 23:41:55 +0100

    ci: New env for BS

--- tests/conftest.py
+++ tests/conftest.py
@@ -123,6 +123,8 @@ def driver(request):
         options.app_activity = ".ApiDemos"
         remote_url = "http://127.0.0.1:4723"
 
+    assert os.getenv ("BROWSERSTACK_USERNAME"), "BROWSERSTACK_USERNAME is empty"
+    assert os.getenv ("BROWSERSTACK_ACCESS_KEY"), "BROWSERSTACK_ACCESS_KEY is empty"
 
     d = webdriver.Remote(remote_url, options=options)
     #   1.	Python отправляет HTTP-запрос на Appium Server
