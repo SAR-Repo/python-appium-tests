699b107b337b998a50a1e3eca5668fda666213db
SAR <sar@intetics.com>
2026-02-06 23:16:50 +0100

    test: Add new manual test

--- test_appium_smoke.py
+++ test_appium_smoke.py
@@ -1,16 +1,18 @@
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 
-options = UiAutomator2Options()
-options.platform_name = "Android"
-options.device_name = "emulator-5554"
-options.app_package = "com.android.settings"
-options.app_activity = ".Settings"
 
-driver = webdriver.Remote(
-    command_executor="http://127.0.0.1:4723",
-    options=options
-)
+def test_open_settings():
+    options = UiAutomator2Options()
+    options.platform_name = "Android"
+    options.device_name = "emulator-5554"
+    options.app_package = "com.android.settings"
+    options.app_activity = ".Settings"
 
-print("App opened")
-driver.quit()
+    driver = webdriver.Remote(
+        command_executor="http://127.0.0.1:4723",
+        options=options
+    )
+
+    print("App opened")
+    driver.quit()
